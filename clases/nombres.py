c2f9062d22a97baff29eb01d6b6590e72892a14c
Alejandro Montes Moreno <alejandromontesmoreno@gmail.com>
2025-09-05 16:04:05 -0600

    versión compartida

---
+++ clases/nombres.py
@@ -0,0 +1,11 @@
+class Nombres:
+    def __init__(self):
+        self.alejandroMontes = "Alejandro Montes"
+    
+    def saludoAlejandroMontes(self):
+        return "Mi nombre es " + self.alejandroMontes
+    
+    '''
+    def saludoTuNombre(self):
+        return "Mi nombre es " + self.propiedadConTuNombre 
+    '''
---
+++ main.py
@@ -0,0 +1,13 @@
+from clases.nombres import Nombres
+
+def main():
+    test = Nombres()
+    
+    #Prueba base
+    print(test.saludoAlejandroMontes())
+    
+    #Realiza aquí tu prueba
+    
+    
+if __name__ == '__main__':
+    main()    
