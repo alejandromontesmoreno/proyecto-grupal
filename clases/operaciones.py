ea620f6c26f5c3ebcc15f77e81d87e348f859338
Mia Horta <miahorta7@gmail.com>
2025-09-09 11:35:59 -0600

    feat: agrega función esPar

--- clases/operaciones.py
+++ clases/operaciones.py
@@ -5,7 +5,11 @@ class Operaciones:
     def saludoAlejandroMontes(self):
         return "Mi nombre es " + self.alejandroMontes
     
-    '''
-    def operacionesAsignada(self):
-        #Realizar la operación asignada
-    '''
+    
+    #Regresar True si el número es par False en caso contrario
+    def esPar(self, numero):
+        if numero % 2 == 0:
+            return True
+        else:
+            return False
+    
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from clases.operaciones import Nombres
-
+from clases.operaciones import Operaciones
 def main():
-    test = Nombres()
+    test = Operaciones()
     
     #Prueba base
-    print(test.saludoAlejandroMontes())
+    #print(test.saludoAlejandroMontes())
     
     #Realiza aquí tu prueba
-    
+    print(test.esPar(4))  
+    print(test.esPar(7))  
     
 if __name__ == '__main__':
     main()    
