0ee9cfaabfead67b1e799dcba44a6bf8cb4405b1
Mauricio Leon <194124-0@iberoleon.edu.mx>
2025-09-09 11:31:40 -0600

    feat: agrega función <Mau>

--- clases/operaciones.py
+++ clases/operaciones.py
@@ -1,11 +1,18 @@
 class Operaciones:
     def __init__(self):
         self.alejandroMontes = "Alejandro Montes"
+        
     
     def saludoAlejandroMontes(self):
         return "Mi nombre es " + self.alejandroMontes
     
-    '''
-    def operacionesAsignada(self):
-        #Realizar la operación asignada
-    '''
+    
+    def esImpar(self,entero):
+        numero = int(entero)
+        if numero %2 != 0:
+            return True
+        else:
+            return False
+
+
+    
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from clases.operaciones import Nombres
+from clases.operaciones import Operaciones
+
 
 def main():
-    test = Nombres()
+    test = Operaciones()
     
     #Prueba base
     print(test.saludoAlejandroMontes())
     
-    #Realiza aquí tu prueba
+    print(test.esImpar(7))
+    print(test.esImpar(4))
     
     
 if __name__ == '__main__':
