17f989071c71ec8d5e54244ffff9c7c3840411f3
Diego Arturo García Mondragón <194155-4@iberoleon.edu.mx>
2025-09-09 11:41:16 -0600

    feat: Promedio <Diego Garcia>

--- clases/operaciones.py
+++ clases/operaciones.py
@@ -1,10 +1,14 @@
 class Operaciones:
+    
     def __init__(self):
         self.alejandroMontes = "Alejandro Montes"
+
     
     def saludoAlejandroMontes(self):
         return "Mi nombre es " + self.alejandroMontes
     
+    def promedioDiegoGarcia(self,lista):
+        return f"El promedio es {sum(lista)/len(lista)}"
     '''
     def operacionesAsignada(self):
         #Realizar la operación asignada
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from clases.operaciones import Nombres
+from clases.operaciones import Operaciones
 
 def main():
-    test = Nombres()
+    test = Operaciones()
     
     #Prueba base
     print(test.saludoAlejandroMontes())
     
     #Realiza aquí tu prueba
-    
+    print(test.promedioDiegoGarcia([1,2,3]))
     
 if __name__ == '__main__':
     main()    
