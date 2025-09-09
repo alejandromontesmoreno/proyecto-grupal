a7eb56868601a54cfcc3381614235e6e9e355b0d
Luis Antonio López <antoniolopezabaunza@gmail.com>
2025-09-09 11:40:03 -0600

    feat:numeroMayo Luis Antonio

--- clases/operaciones.py
+++ clases/operaciones.py
@@ -5,7 +5,12 @@ class Operaciones:
     def saludoAlejandroMontes(self):
         return "Mi nombre es " + self.alejandroMontes
     
-    '''
-    def operacionesAsignada(self):
-        #Realizar la operación asignada
-    '''
+    
+    def numeroMayo(self,a,b,c):
+        mayor=a
+        if (b>mayor):
+            mayor=b
+        if (c>mayor):
+            mayor=c
+        return mayor
+    
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
-from clases.operaciones import Nombres
+from clases.operaciones import Operaciones
 
 def main():
-    test = Nombres()
+    test = Operaciones()
     
     #Prueba base
     print(test.saludoAlejandroMontes())
     
     #Realiza aquí tu prueba
-    
+    a=float(input('dame el primer número: '))
+    b=float(input('dame el segundo número: '))
+    c=float(input('dame el tercer número: '))
+    print(test.numeroMayo(a,b,c))
     
 if __name__ == '__main__':
     main()    
